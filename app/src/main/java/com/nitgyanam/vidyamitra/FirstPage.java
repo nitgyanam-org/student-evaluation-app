cded47a37768c44f782b791ee6a54e7fdbd742c7
Aryan Nayan <aryannayan@Aryans-Mac.local>
2025-12-24 02:53:48 +0530

    Added firstPage and LoginPage

---
+++ app/src/main/java/com/nitgyanam/vidyamitra/FirstPage.java
@@ -0,0 +1,28 @@
+package com.nitgyanam.vidyamitra;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        Button signupBtn = findViewById(R.id.signupBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
+        signupBtn.setOnClickListener(v -> {
+//            startActivity(new Intent(this, SignupActivity.class));
+        });
+
+    }
+}
