18d03d4818497eb55a70d7ce4bb3dff158ea1320
Tejash Kr. <spaciitks@gmail.com>
2025-12-25 22:12:06 +0530

    Added student dashboard and profile page

---
+++ app/src/main/java/com/nitgyanam/vidyamitra/StudentProfileActivity.java
@@ -0,0 +1,17 @@
+package com.nitgyanam.vidyamitra;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StudentProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
