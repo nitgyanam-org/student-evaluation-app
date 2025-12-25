4969776540b8de96ef097da1346ac639002973d4
Tejash Kr. <spaciitks@gmail.com>
2025-12-25 15:09:35 +0530

    Student dashboard added and login navigation fixed

---
+++ app/src/main/java/com/nitgyanam/vidyamitra/StudentDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.nitgyanam.vidyamitra;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dashboard);
+    }
+}
